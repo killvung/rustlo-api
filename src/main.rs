f6439c3c97de35b0c5d72d4bfc616942e7d97f0a
Kiu Fung Lam <killvung@gmail.com>
2020-07-19 17:15:43 -0500

    Wrap index to index scope

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,65 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::get;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+#[get("/stateCounter")]
+async fn _index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
+}
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/state")]
+async fn index4(data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {}!", app_name) // <- response with app_name
+}
 
 #[get("/hello")]
 async fn index3() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/again")]
 async fn index2() -> impl Responder {
     HttpResponse::Ok().body("Hello world again!")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
-            .service(index3)
+            .data(AppState {
+                app_name: String::from("hello-world-api"),
+            })
+            .app_data(counter.clone()) // <- register the created data
+            .service(
+                web::scope("/index")
+                    .service(index)
+                    .service(index2)
+                    .service(index3)
+                    .service(index4)
+                    .service(_index),
+            )
     })
     .bind("127.0.0.1:8088")?
     .run()
